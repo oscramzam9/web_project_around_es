49aebad9444b89936a8855c3ebf14c39359f9e42
OscarR <oscramzam9@gmail.com>
2026-03-01 23:22:19 +0800

    add functionality to print 8 part 2

--- scripts/index.js
+++ scripts/index.js
@@ -28,3 +28,49 @@ const initialCards = [
 initialCards.forEach(function (card) {
   console.log(card.name);
 });
+
+const modalProfile = document.querySelector("#edit-popup");
+//Create button variables.
+const editButton = document.querySelector(".profile__edit-button");
+const editPopup = document.querySelector("#edit-popup");
+const closeButton = editPopup.querySelector(".popup__close");
+
+//User Data
+const profileForm = document.querySelector("#edit-profile-form");
+const profileName = document.querySelector(".profile__title");
+const profileJob = document.querySelector(".profile__description");
+
+//Form input
+const nameInput = document.querySelector(".popup__input_type_name");
+const jobInput = document.querySelector(".popup__input_type_description");
+
+function openModal(modal) {
+  modal.classList.add("popup_is-opened");
+}
+
+function closeModal(popup) {
+  popup.classList.remove("popup_is-opened");
+}
+
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileJob.textContent = jobInput.value;
+  closeModal(modalProfile);
+}
+
+function openEditProfile() {
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
+  openModal(editPopup);
+}
+editButton.addEventListener("click", openEditProfile);
+
+function closeEditProfile() {
+  closeModal(editPopup);
+}
+closeButton.addEventListener("click", closeEditProfile);
+
+profileForm.addEventListener("submit", handleProfileFormSubmit);
+
+setEventListeners(profileForm);
