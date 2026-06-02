7c74ef975ee69dd498afba6aa5315684df4abd20
OscarRZ <oscramzam9@gmail.com>
2026-06-01 18:52:16 -0600

    Update paths

--- src/page/index.js
+++ src/page/index.js
@@ -1,12 +1,12 @@
 ////////////////////////////// Begin of imports ////////////////////////////////
-import Api from "./Api.js";
-import { Card } from "./Card.js";
-import { FormValidator } from "./FormValidator.js";
-import PopupWithConfirmation from "./PopupWithConfirmation.js";
-import PopupWithForm from "./PopupWithForm.js";
-import PopupWithImage from "./PopupWithImage.js";
-import Section from "./Section.js";
-import UserInfo from "./UserInfo.js";
+import Api from "../components/Api.js";
+import { Card } from "../components/Card.js";
+import { FormValidator } from "../components/FormValidator.js";
+import PopupWithConfirmation from "../components/PopupWithConfirmation.js";
+import PopupWithForm from "../components/PopupWithForms.js";
+import PopupWithImage from "../components/PopupWithImage.js";
+import Section from "../components/Section.js";
+import UserInfo from "../components/UserInfo.js";
 ///////////////////////////// End of imports ///////////////////////////////////
 
 ////////////////////////////// Variable definition ///////////////////////////////
